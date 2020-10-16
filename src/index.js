23a010debb04a976464ce2e96f67698135b940c2
miguel <miguelcoria94@gmail.com>
2020-10-15 21:24:46 -0500

    added my first react element!

--- src/index.js
+++ src/index.js
@@ -6,7 +6,13 @@ import ReactDOM from 'react-dom';
 
 //create a react component
 
-
+const App = () => {
+    return <div>Hi there! This is a react app!</div>
+}
 
 // take the react component and show on screen
 
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
