ae9a04d6e1abcd187cba35ff31e3f075bd3f2d9b
miguel <miguelcoria94@gmail.com>
2020-10-15 21:22:50 -0500

    import

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// import the react and reactdom libraries
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+//create a react component
+
+
+
+// take the react component and show on screen
+
